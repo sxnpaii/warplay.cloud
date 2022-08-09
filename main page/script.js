cd91d669e33729e756be452140850aaae104f771
Khurshid <axurshidbek2005@gmail.com>
2022-08-09 18:42:44 +0500

    warplay cloud

---
+++ main page/script.js
@@ -0,0 +1,11 @@
+let con = document.getElementsByClassName(' .container  ');
+let colon = document.getElementById('card')
+con.onmouseover = function() {
+    colon.classList.add('hover')
+}
+
+// con.addEventListener('click', function() {
+//     con.classList.add('hover')
+// })
+
+// alert('worked')
